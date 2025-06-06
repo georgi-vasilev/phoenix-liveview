4732e8fe23b52f2260f938ad13cd59cebea68ee5
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-06-06 14:35:05 +0300

    update: remove header from layout

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -2,8 +2,20 @@ defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
 
   def render(assigns) do
+    person = %{age: 17, name: "Peter"}
+    # this should be done in mount/3
+    assigns = assign(assigns, :person, person)
+
+
     ~H"""
     <div>Welcome to the chat!</div>
+    <div>
+      <%= if @person.age >= 18 do %>
+        <span> Adult </span>
+      <% else %>
+        <span> Child </span>
+      <% end %>
+    </div>
     """
   end
 end
