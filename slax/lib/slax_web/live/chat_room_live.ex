2e72a363280d2d475671399a8bc841c8900f4812
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-06-06 16:25:41 +0300

    feature: use patch instead of navigate

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -65,16 +65,23 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
 
-  def mount(params, _session, socket) do
+  def mount(_params, _session, socket) do
     rooms = Repo.all(Room)
 
+    {:ok, assign(socket, rooms: rooms)}
+  end
+
+  def handle_params(params, _uri, socket) do
     room =
       case Map.fetch(params, "id") do
-        {:ok, id} -> Repo.get!(Room, id)
-        :error -> List.first(rooms)
+        {:ok, id} ->
+          Repo.get!(Room, id)
+
+        :error ->
+          List.first(socket.assigns.rooms)
       end
 
-    {:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
+    {:noreply, assign(socket, room: room, hide_topic?: false)}
   end
 
   def handle_event("toggle-topic", _params, socket) do
