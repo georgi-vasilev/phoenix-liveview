96094457bf7c3fce45faea1a2786d9a865f5e6d4
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-06-16 09:11:13 +0300

    feature: add 'Join Room' button and restrict room editing

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -66,6 +66,7 @@ defmodule SlaxWeb.ChatRoomLive do
           <h1 class="text-sm font-bold leading-none">
             #{@room.name}
             <.link
+              :if={@joined?}
               class="font-normal text-xs text-blue-600 hover:text-blue-700"
               navigate={~p"/rooms/#{@room}/edit"}
             >
@@ -142,6 +143,34 @@ defmodule SlaxWeb.ChatRoomLive do
             <.icon name="hero-paper-airplane" class="h-4 w-4" />
           </button>
         </.form>
+      </div>
+      <div
+        :if={!@joined?}
+        class="flex justify-around mx-5 mb-5 p-6 bg-slate-100 border-slate-300 border-rounded-lg"
+      >
+        <div class="max w-3-xl text-center">
+          <div class="mb-4">
+            <h1 class="text-xl font-semibold">#{@room.name}</h1>
+            <p :if={@room.topic} class="text-sm mt-1 text-gray-600">{@room.topic}</p>
+          </div>
+          <div class="flex items-center justify-around">
+            <button
+              phx-click="join-room"
+              class="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus: ring-green-500"
+            >
+              Join Room
+            </button>
+          </div>
+          <div class="m5-4">
+            <.link
+              navigate={~p"/rooms"}
+              href="#"
+              class="text-sm text-slate-500 underline hover:text-slate-600"
+            >
+              Back to All Rooms
+            </.link>
+          </div>
+        </div>
       </div>
     </div>
     """
@@ -344,6 +373,14 @@ defmodule SlaxWeb.ChatRoomLive do
   def handle_event("delete-message", %{"id" => message_id}, socket) do
     Chat.delete_message(message_id, socket.assigns.current_user)
 
+    {:noreply, socket}
+  end
+
+  def handle_event("join-room", _, socket) do
+    current_user = socket.assigns.current_user
+    Chat.join_room!(socket.assigns.room, current_user)
+    Chat.subscribe_to_room(socket.assigns.room)
+    socket = assign(socket, joined?: true, rooms: Chat.list_joined_rooms(current_user))
     {:noreply, socket}
   end
 
