46388ef5071141e91304e5373735587894b12406
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-06-18 09:32:55 +0300

    feature: extract UserComponents

--- slax/lib/slax_web/live/chat_room_live.ex
+++ slax/lib/slax_web/live/chat_room_live.ex
@@ -8,6 +8,8 @@ defmodule SlaxWeb.ChatRoomLive do
   alias Slax.Chat.Room
   alias SlaxWeb.OnlineUsers
 
+  import SlaxWeb.UserComponents
+
   def render(assigns) do
     ~H"""
     <div class="flex flex-col shrink-0 w-64 bg-slate-100">
@@ -103,7 +105,7 @@ defmodule SlaxWeb.ChatRoomLive do
                 phx-click="show-profile"
                 phx-value-user-id={@current_user.id}
               >
-                <img src={user_avatar_path(@current_user)} class="h-8 w-8 rounded" />
+                <.user_avatar user={@current_user} class="h-8 w-8 rounded" />
                 <span class="hover:underline">{@current_user.username}</span>
               </.link>
             </div>
@@ -282,11 +284,11 @@ defmodule SlaxWeb.ChatRoomLive do
       >
         <.icon name="hero-trash" class="h-4 w-4" />
       </button>
-      <img
+      <.user_avatar
+        user={@message.user}
         class="h-10 w-10 rounded cursor-pointer"
         phx-click="show-profile"
         phx-value-user-id={@message.user.id}
-        src={user_avatar_path(@message.user)}
       />
       <div class="ml-2">
         <div class="-mt-1">
@@ -305,14 +307,6 @@ defmodule SlaxWeb.ChatRoomLive do
       </div>
     </div>
     """
-  end
-
-  defp user_avatar_path(user) do
-    if user.avatar_path do
-      ~p"/uploads/#{user.avatar_path}"
-    else
-      ~p"/images/one_ring.jpg"
-    end
   end
 
   attr :count, :integer, required: true
--- slax/lib/slax_web/live/chat_room_live/profile_component.ex
+++ slax/lib/slax_web/live/chat_room_live/profile_component.ex
@@ -1,6 +1,8 @@
 defmodule SlaxWeb.ChatRoomLive.ProfileComponent do
   use SlaxWeb, :live_component
 
+  import SlaxWeb.UserComponents
+
   def render(assigns) do
     ~H"""
     <div class="flex flex-col shrink-0 w-1/4 max-w-xs bg-white shadow-xl">
@@ -19,7 +21,7 @@ defmodule SlaxWeb.ChatRoomLive.ProfileComponent do
       </div>
       <div class="flex flex-col grow overflow-auto p-4">
         <div class="mb-4">
-          <img src={user_avatar_path(@user)} class="w-48 rounded mx-auto" />
+          <.user_avatar user={@user} class="w-48 rounded mx-auto" />
         </div>
         <h2 class="text-xl font-bold text-gray-800">
           {@user.username}
@@ -27,13 +29,5 @@ defmodule SlaxWeb.ChatRoomLive.ProfileComponent do
       </div>
     </div>
     """
-  end
-
-  defp user_avatar_path(user) do
-    if user.avatar_path do
-      ~p"/uploads/#{user.avatar_path}"
-    else
-      ~p"/images/one_ring.jpg"
-    end
   end
 end
