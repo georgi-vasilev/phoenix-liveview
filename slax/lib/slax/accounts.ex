f4be1d104a554bf0bcbffc749618c4a0c01b3a45
Georgi Vasilev <georgisvasilevv@gmail.com>
2025-06-18 11:26:23 +0300

    feature: broadcast avatar updates with PubSub

--- slax/lib/slax/accounts.ex
+++ slax/lib/slax/accounts.ex
@@ -8,6 +8,10 @@ defmodule Slax.Accounts do
 
   alias Slax.Accounts.{User, UserNotifier, UserToken}
 
+  @pubsub Slax.PubSub
+
+  @user_avatar_topic "user-avatars"
+
   ## Database getters
 
   @doc """
@@ -366,8 +370,17 @@ defmodule Slax.Accounts do
   end
 
   def save_user_avatar_path(user, avatar_path) do
-    user
-    |> User.avatar_changeset(%{avatar_path: avatar_path})
-    |> Repo.update()
+    with {:ok, user} <-
+           user
+           |> User.avatar_changeset(%{avatar_path: avatar_path})
+           |> Repo.update() do
+      Phoenix.PubSub.broadcast(@pubsub, @user_avatar_topic, {:updated_avatar, user})
+    end
+
+    {:ok, user}
+  end
+
+  def subscribe_to_user_avatars do
+    Phoenix.PubSub.subscribe(@pubsub, @user_avatar_topic)
   end
 end
